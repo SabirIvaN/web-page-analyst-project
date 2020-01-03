4ee4e7d872c3ba4572f1edd3db8ba71b68aad529
Иван Сабиров <sabirivan@yandex.com>
2020-01-04 01:17:46 +0400

    Подключен debugbar

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -86,6 +86,8 @@ if (env('APP_DEBUG')) {
     $app->register(Barryvdh\Debugbar\LumenServiceProvider::class);
 }
 
+$app->configure('debugbar');
+
 /*
 |--------------------------------------------------------------------------
 | Load The Application Routes
