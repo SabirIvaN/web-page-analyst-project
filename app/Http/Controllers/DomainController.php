2c002dd40671999845b0a6196176de3d3cf3970b
Иван Сабиров <sabirivan@yandex.com>
2020-02-09 19:30:57 +0300

    Fifth step is complete

--- app/Http/Controllers/DomainController.php
+++ app/Http/Controllers/DomainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Laravel\Lumen\Routing\Controller as BaseController;
@@ -27,12 +28,22 @@ class DomainController extends BaseController
 
     public function sendData(Request $request)
     {
-        $url = $request->input("urlSiteInputing");
+        $url = $request->input('urlSiteInputing');
+
+        $client = new Client();
+        $response = $client->request('GET', $url);
+        $contentLength = $response->getBody()->getSize();
+        $responseCode = $response->getStatusCode();
+        $body = $response->getBody();
+
         $currentDateTime = date('d/M/Y H:i:s');
         $id = DB::table('domains')->insertGetId([
             'name' => $url,
             'updated_at' => $currentDateTime,
-            'created_at' => $currentDateTime
+            'created_at' => $currentDateTime,
+            'content_length' => $contentLength,
+            'response_code' => $responseCode,
+            'body' => $body
         ]);
         return redirect()->route('domains.table', ['id' => $id]);
     }
--- resources/views/domains/table.blade.php
+++ resources/views/domains/table.blade.php
@@ -16,6 +16,8 @@
           <th scope="col">URL</th>
           <th scope="col">Updated at</th>
           <th scope="col">Created at</th>
+          <th scope="col">Content length</th>
+          <th scope="col">Response code</th>
         </tr>
     </thead>
     <tbody>
@@ -23,6 +25,8 @@
           <td>{{ $domain->name }}</td>
           <td>{{ $domain->created_at }}</td>
           <td>{{ $domain->updated_at }}</td>
+          <td>{{ $domain->content_length }}</td>
+          <td>{{ $domain->response_code }}</td>
         </tr>
     </tbody>
 </table>
